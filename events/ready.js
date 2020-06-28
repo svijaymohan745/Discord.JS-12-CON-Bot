b8307faa06c087baff32f7e04f04607524fd9822
nevinbaiju <nevinbaiju@gmail.com>
2020-06-28 23:57:37 +0530

    ready event

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,22 @@
 module.exports = (client) => {
-    console.log(`This is: ${client.user.tag}`);
+    var testChannel = client.channels.cache.find(channel => channel.id === '716557043628245025');
+    console.log('Welcome me into this nasty World');
+    client.user.setActivity('your darkest secrets [_help]', { type: 'LISTENING'}).catch(console.error);
+
+    setInterval(() => {
+        testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+    }, 604800000);
+    setInterval(() => {
+        testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+    }, 691200000);
+    setInterval(() => {
+        testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+    }, 777600000);
+    setInterval(() => {
+        testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+    }, 864000000);
+    setInterval(() => {
+        testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+    }, 950400000);
     
 }
