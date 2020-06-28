1ec15211085f1a016466f18da7ea5e7eeca22598
nevinbaiju <nevinbaiju@gmail.com>
2020-06-28 19:15:29 +0530

    added meme to commands

---
+++ commands/meme.js
@@ -0,0 +1,26 @@
+const randomPuppy = require("random-puppy");
+const { MessageEmbed  } = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    const subReddits = ["dankmeme", "dankmemes","memes", "meme", "jokes", "joke", "me_irl"];
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+    const meme = await randomPuppy(`${random}`).catch((error) => {
+        console.error(error);
+    });
+    const embed = new MessageEmbed().setColor("RANDOM").setImage(meme)
+    .setTitle(`From /r/${random}`)
+    .setURL(`https://www.reddit.com/r/${random}`);
+    message.channel.send(embed)
+
+}
+
+module.exports.help = {
+    name: "meme",
+    description: "Just a Meme"
+}
+
+module.exports.requirements = {
+    userPerms: [],
+    clientPerms: [],
+    ownerOnly: false
+}
