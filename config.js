1850420e72839f4db80863f579887dd1be2ef1b6
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-06-30 04:02:46 +0530

    ignore

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.token = "TOKEN_HERE";
-exports.owners = [ID_HERE];
+exports.token = "NzIwOTI4MTQyMTA5MTc5OTU0.XvciSg.Rg9WYy5OELlzjYjfswkatMUXs_0";
+exports.owners = [214291790566522881];
 exports.prefix = "_";
