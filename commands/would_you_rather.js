742f26460f7510d038be6acf6de8b944d6b5bb77
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-07-05 03:17:15 +0530

    Added Would You Rather Command

---
+++ commands/would_you_rather.js
@@ -0,0 +1,40 @@
+const questions = require('../messages/wyr/would-you-rather.json');
+const { MessageEmbed  } = require("discord.js");
+const Discord = require('discord.js');
+
+module.exports.run =  async (client, message, args) => {
+
+
+    var messagetext =  questions[Math.floor(Math.random() * questions.length)]
+    var question = messagetext.split("Would you rather ")[1]
+    var Option1 = question.split(" or ")[0]
+    var Option2 = question.split(" or ")[1]
+
+    reply = {
+        embed: {
+            color: 3447003,
+            author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+            },
+            "title": "Help Commands",
+            "description": `Would you rather \n 🅰️ ${Option1} \n or \n :regional_indicator_b: ${Option2}`,
+           
+        },
+    }
+    wyrmessage = await message.channel.send(reply);
+    wyrmessage.react('🅰️')
+    wyrmessage.react('🇧')
+
+
+}
+module.exports.help = {
+    name: "wyr",
+    description: "The Would you Rather 3:) game"
+}
+
+module.exports.requirements = {
+    userPerms: [],
+    clientPerms: [],
+    ownerOnly: false
+}
