07d3c41ac788d62bda5c8d8897ecd25809d0f226
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-07-10 22:02:26 +0530

    modified poll command

--- commands/poll.js
+++ commands/poll.js
@@ -1,22 +1,32 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args, level) => {
+  if (message.content.startsWith("_poll")) {
+    message.delete({ timeout: 0.5 });
+  }
+  let reactionNumber = Number(args[0]);
     try {
-      if (!args.join(' ')) return message.reply('You need to supply the question');
-      
+      if (!args.join(' ')) return message.reply('You need to supply the question');   
       let embed = new Discord.MessageEmbed()
-      .setTitle(args.join(' '))
-      .setDescription('Poll created by ' + message.author.tag)
-      .setColor('#eeeeee');
-  
+      .setTitle(args[1])
+      .setFooter(`select the ${reactionNumber} reactions below `)
+      .setImage(args[2])
+      .setColor('#eeeeee')
+      .setDescription(":thumbsup: =" + args[3] +  "\n:thumbsdown:  = " + args[4] )
       let msg = await message.channel.send(embed);
-      
-      await msg.react('👍');
-      await msg.react('👎');
+      reactions = ["👍", "👎", "🅰️"]
+      for (i = 0; i < reactionNumber; i++){
+        await msg.react(reactions[i])
+      }
+
     } catch (err) {
       message.channel.send('There was an error!\n' + err).catch();
     }
+       
+
+
   };
+  
 module.exports.help = {
     name: "poll",
     description: "Create a poll",
